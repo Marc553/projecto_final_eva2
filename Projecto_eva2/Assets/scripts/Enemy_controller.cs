c4c7b47b4be8838f9ac7de8a121f2c3c8826d8ea
Marc553 <91455235+Marc553@users.noreply.github.com>
2022-03-14 10:39:33 +0100

    14/03/2022

--- Projecto_eva2/Assets/scripts/Enemy_controller.cs
+++ Projecto_eva2/Assets/scripts/Enemy_controller.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class Enemy_controller : MonoBehaviour
 {
     private Rigidbody enemyRigidbody;
-    public GameObject player;
+    private GameObject player;
     [SerializeField] private float speed = 5f;
     
 
     void Start()
     {
         enemyRigidbody = GetComponent<Rigidbody>();
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
-    {/*
+    {
         Vector3 direction = (player.transform.position - transform.position).normalized;
-        enemyRigidbody.AddForce(direction * speed);*/
+        enemyRigidbody.AddForce(direction * speed);
 
     }
 
--- Projecto_eva2/Assets/scripts/SpawnManger.cs
+++ Projecto_eva2/Assets/scripts/SpawnManger.cs
@@ -3,29 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManger : MonoBehaviour
-{/*
+{
     public GameObject[] objectsPrefabs; 
-    private Vector3 SpawnPosition;
-    private int randomPosition;
-
-    private int valor0 = 0;
-    private int valor3 = 3;
-    private float giro = 0;
-    private float giro90 = 90;
-    private float giro180 = 180;
-    private float giroMenus90 = -90;
-   
-    private float valorPlusX = 37;
-    private float valorMInusX = -37;
-    private float valorPlusZ = 34.6f;
-    private float valorMinusZ = -34.6f;
-
+    public GameObject[] way_points; 
+    public GameObject[] particle_points; 
+    private GameObject SpawnPosition;
+    private GameObject SpawnParticle;
+  
     private int enemiesLeft;
     private int enemiesPerWave = 1;
   
-
     private int randomIndex;
+    private int randomPosition;
+    private int randomParticle;
 
+    public ParticleSystem warning;
 
     void Start()
     {
@@ -34,8 +26,8 @@ public class SpawnManger : MonoBehaviour
 
     private void Update()
     {
-        enemiesLeft = FindObjectOfType<Enemy_controller>().Length;
-        if(enemiesLeft <= valor0)
+        enemiesLeft = FindObjectsOfType<Enemy_controller>().Length;
+        if(enemiesLeft <= 0)
         {
             enemiesPerWave++;
             SpwamEnemyWave(enemiesPerWave);
@@ -54,32 +46,15 @@ public class SpawnManger : MonoBehaviour
     public void SpawnEnemy()
     {
         randomIndex = Random.Range(0, objectsPrefabs.Length);
-        randomPosition = Random.Range(valor0, valor3);
+        randomPosition = Random.Range(0, way_points.Length);
+        
 
-        if (randomPosition == valor0)
-        {
-             SpawnPosition = new Vector3(valorPlusX, valor0, valor0);
-            Quaternion rotation = Quaternion.Euler(transform.rotation.x, giro, transform.rotation.z);
-            Instantiate(objectsPrefabs[randomIndex], SpawnPosition, rotation);
-        }
-        if (randomPosition == 1)
-        {
-            SpawnPosition = new Vector3(valorMInusX, valor0, valor0);
-            Quaternion rotation = Quaternion.Euler(transform.rotation.x, giro180, transform.rotation.z);
-            Instantiate(objectsPrefabs[randomIndex], SpawnPosition, rotation);
-        }
-        if (randomPosition == 2)
-        {
-            SpawnPosition = new Vector3(valor0, valor0, valorPlusZ);
-            Quaternion rotation = Quaternion.Euler(transform.rotation.x, giroMenus90, transform.rotation.z);
-            Instantiate(objectsPrefabs[randomIndex], SpawnPosition, rotation);
-        }
-        if (randomPosition == valor3)
-        {
-            SpawnPosition = new Vector3(valor0, valor0, valorMinusZ);
-            Quaternion rotation = Quaternion.Euler(transform.rotation.x, giro90, transform.rotation.z);
-            Instantiate(objectsPrefabs[randomIndex], SpawnPosition, rotation);
-        }
-    }*/
+        SpawnPosition = way_points[randomPosition];
+        SpawnParticle = particle_points[randomPosition];
+        Instantiate(objectsPrefabs[randomIndex], SpawnPosition.transform.position, SpawnPosition.transform.rotation);
+        Instantiate(warning, SpawnParticle.transform.position, SpawnParticle.transform.rotation);
+
+        
+    }
 
 }
