11f4b63ec8168b777602fc5c395347dd5d34ccd8
Marc553 <91455235+Marc553@users.noreply.github.com>
2022-03-21 11:22:19 +0100

    21/03/2022

--- Projecto_eva2/Assets/scripts/Enemy_controller.cs
+++ Projecto_eva2/Assets/scripts/Enemy_controller.cs
@@ -9,23 +9,31 @@ public class Enemy_controller : MonoBehaviour
     [SerializeField] private float speed = 5f;
     private int limY = -3;
     private int playerLeft;
+    public GameManager gameManagerScript;
 
 
     void Start()
     {
         enemyRigidbody = GetComponent<Rigidbody>();
-        
-            player = GameObject.Find("Player");
-        
 
+        gameManagerScript = FindObjectOfType<GameManager>();
+        
+        if(!gameManagerScript.isGameOver)
+        {
+            player = GameObject.Find("Player");
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 direction = (player.transform.position - transform.position).normalized;
-        enemyRigidbody.AddForce(direction * speed);
+        if (!gameManagerScript.isGameOver)
+        { 
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+             enemyRigidbody.AddForce(direction * speed);
+        }
+           
 
         if(transform.position.y <= limY)
         {
--- Projecto_eva2/Assets/scripts/GameManager.cs
+++ Projecto_eva2/Assets/scripts/GameManager.cs
@@ -21,28 +21,47 @@ public class GameManager : MonoBehaviour
     private int randomParticle;
     private float spawnPositionRange = 30;
 
-    public TextMeshProUGUI pointsText;
+    public AudioSource audioSource;
+    public AudioClip menu;
+    public AudioClip juego;
+    public AudioClip muerte;
+    //public TextMeshProUGUI pointsText;
     public GameObject gameOverPanel;
     public GameObject mainMenuPanel;
 
-    public bool isGameOver;
+    public bool isGameOver = true;
 
     public ParticleSystem warning;
 
     void Start()
-    {
+    { 
+        audioSource = GetComponent<AudioSource>();
+        isGameOver = true;
         mainMenuPanel.SetActive(true);
+        
+        audioSource.clip = menu;
+        audioSource.loop = true;
+        audioSource.Play();
+
+       
     }
 
     private void Update()
     {
+        if(!isGameOver)
+        {
+            
             enemiesLeft = FindObjectsOfType<Enemy_controller>().Length;
+           
             if (enemiesLeft <= 0)
             {
              enemiesPerWave++;
              SpwamEnemyWave(enemiesPerWave);
              Instantiate(powerUpPrefab, RandomSpawnPostion(), powerUpPrefab.transform.rotation);
             }
+
+        }
+            
     }
 
     private void SpwamEnemyWave(int totalenemies)
@@ -59,7 +78,7 @@ public class GameManager : MonoBehaviour
          randomPosition = Random.Range(0, way_points.Length);
          SpawnPosition = way_points[randomPosition];
          SpawnParticle = particle_points[randomPosition];
-         Instantiate(objectsPrefabs[randomIndex], SpawnPosition.transform.position, SpawnPosition.transform.rotation);
+        Instantiate(objectsPrefabs[randomIndex], SpawnPosition.transform.position, SpawnPosition.transform.rotation);
         Instantiate(warning, SpawnParticle.transform.position, SpawnParticle.transform.rotation);   
     }
 
@@ -73,8 +92,10 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        audioSource.Stop();
         isGameOver = true;
         gameOverPanel.SetActive(true);
+        audioSource.PlayOneShot(muerte);
     }
 
     public void RestartGame()
@@ -84,9 +105,30 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
+        audioSource.Stop();
+
+        audioSource.clip = juego;
+        audioSource.loop = true;
+        audioSource.Play();
+
         mainMenuPanel.SetActive(false);
         isGameOver = false;
         gameOverPanel.SetActive(false);
+        enemiesPerWave = 1;
         SpwamEnemyWave(enemiesPerWave);
     }
+
+    public void Exit()
+    { 
+        #if UNITY_EDITOR
+    
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+
+    }
+    // (salir del juego)
+    
+
+
 }
